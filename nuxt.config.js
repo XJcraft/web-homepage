cf890ad409f84ab627757ebeee7f35e2c1a860e3
jim_ <769925821@qq.com>
2020-04-04 03:23:03 +0800

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'universal',
+  srcDir: path.resolve(__dirname, 'src', 'client'),
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: ['element-ui/lib/theme-chalk/index.css'],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: ['@/plugins/element-ui'],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [],
+
+  buildDir: path.resolve(__dirname, 'dist', 'client'),
+  /*
+   ** Build configuration
+   */
+  build: {
+    transpile: [/^element-ui/],
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+}
